f513c9e4524d8f6648466f776989e77f3da07eee
babidiprogramming <prccabanducos97@gmail.com>
2025-05-24 16:45:34 +0800

    dynamic text

--- script.js
+++ script.js
@@ -18,6 +18,30 @@ navLinkEls.forEach(navLink =>{
 });
 });
 
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('.sidebar').classList.toggle('active');
-});
+
+/*-------------Dynamic Text--------------*/
+const dynamicText = document.querySelector(".section_text_p2");
+const words = ["🎨Design is not just what it looks like and feels like. Design is how it works.",
+  "🧠By failing to prepare, you are preparing to fail."];
+let wordIndex = 0;
+let charIndex = 1;
+let isDeleting = false;
+const typeEffect = () =>{
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+
+  if(!isDeleting && charIndex < currentWord.length){
+    charIndex++;
+    setTimeout(typeEffect, 30);
+  }else if(isDeleting && charIndex > 0){
+    charIndex--;
+    setTimeout(typeEffect, 30);
+  }else{
+    isDeleting = !isDeleting;
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 500);
+  }
+
+}
+typeEffect();
